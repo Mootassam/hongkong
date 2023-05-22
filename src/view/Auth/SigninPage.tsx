ece488cbe626df6d150e85a42a2f9bb44b2e0a2f
Mootassam <mootassame@gmail.com>
2023-05-22 07:46:37 +0000

    Send Request from signipage for singnin

--- src/view/Auth/SigninPage.tsx
+++ src/view/Auth/SigninPage.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Styles/styles.css";
 import { useDispatch, useSelector } from "react-redux";
-
+import actions from "../../modules/auth/authActions";
 
 function SigninPage() {
-  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const dispatch = useDispatch();
+  const onSubmitForm = () => {
+    dispatch(actions.doSigninWithEmailAndPassword(email, password, true));
+  };
   return (
     <div className="app__singnin">
       <div className="singin__page">
@@ -13,41 +18,42 @@ function SigninPage() {
           <h1>Sign In</h1>
           <span>Let’s build something greate</span>
         </div>
-        <form  >
-          <div className="singin__form">
-            <div className="singin__group">
-              <label htmlFor="">E-mail or phone number</label>
-              <input
-                type="text"
-                placeholder="Enter your Email"
-                className="singin__input"
-              />
-            </div>
 
-            <div className="singin__group">
-              <label htmlFor="">Password</label>
-              <input
-                type="text"
-                placeholder="Enter your passowrd"
-                className="singin__input"
-              />
-            </div>
-
-            <div className="singin__button">
-              <span>Login</span>
-            </div>
-
-            <div className="singin__donthaveaccount">
-              <span>
-                Don’t have an account?{" "}
-                <label htmlFor="" className="signup__link">
-                  {" "}
-                  Sign up
-                </label>
-              </span>
-            </div>
+        <div className="singin__form">
+          <div className="singin__group">
+            <label htmlFor="">E-mail or phone number</label>
+            <input
+              type="text"
+              placeholder="Enter your Email"
+              className="singin__input"
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </div>
-        </form>
+
+          <div className="singin__group">
+            <label htmlFor="">Password</label>
+            <input
+              type="text"
+              placeholder="Enter your passowrd"
+              className="singin__input"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+
+          <button onClick={() => onSubmitForm()} className="singin__button">
+            <span>Login</span>
+          </button>
+
+          <div className="singin__donthaveaccount">
+            <span>
+              Don’t have an account?{" "}
+              <label htmlFor="" className="signup__link">
+                {" "}
+                Sign up
+              </label>
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
